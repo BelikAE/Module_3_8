925a3ec0284ef27d18f607e61709873e5860a87d
BelikAE <BelikAE@HOME-PC>
2024-07-15 19:06:11 +1000

    Выдача

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Users\BelikAE\Desktop\C#";
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach (string file in files) 
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Lab2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string path = @"C:\Users\BelikAE\Desktop\Lab2.txt";
+            Random random = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(-100, 100));
+                }
+
+            }
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += Convert.ToInt32(sr.ReadLine());
+                    
+                }
+
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
